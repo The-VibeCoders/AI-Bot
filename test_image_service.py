0ee37f5698eb537db4f02d192e5e0ed913e810e9
Kaustubh Divekar <divekarkaustubh492@gmail.com>
2026-05-08 11:24:58 +0530

    Add files via upload

---
+++ test_image_service.py
@@ -0,0 +1,10 @@
+﻿import sys
+sys.path.append("C:\\Users\\divek\\Desktop\\mini project v2")
+from app.services.image_service import ImageService
+
+service = ImageService()
+print("Service initialized. Device:", service.device)
+prompt = "A simple red circle"
+message, filename = service.draw(prompt)
+print("Message:", message)
+print("Filename:", filename)
